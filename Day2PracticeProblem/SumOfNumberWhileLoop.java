a43ed15568c7bbd6307d2ca8e0555f3281deb8ab
priyanshusuman9 <105853154+priyanshusuman9@users.noreply.github.com>
2022-11-17 09:44:31 +0530

    commit

---
+++ Day2PracticeProblem/SumOfNumberWhileLoop.java
@@ -0,0 +1,20 @@
+package Class1.src.com.RFP_Batch225.Day2PracticeProblem;
+
+import java.util.Scanner;
+public class SumOfNumberWhileLoop
+{
+    public static void main(String args[])
+    {
+        int x, i = 1 ;
+        int sum = 0;
+        System.out.println("Enter Number of items :");
+        Scanner s = new Scanner(System.in);
+        x = s.nextInt();
+        while(i <= x)
+        {
+            sum = sum +i;
+            i++;
+        }
+        System.out.println("Sum of "+x+" numbers is :"+sum);
+    }
+}
