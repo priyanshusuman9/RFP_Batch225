7512513fb2b6ca9df6c32011ddc415ce407bbc1f
priyanshusuman9 <105853154+priyanshusuman9@users.noreply.github.com>
2022-11-17 08:19:44 +0530

    Add files via upload

---
+++ Day2PracticeProblem/SumoFNumber.java
@@ -0,0 +1,17 @@
+package Class1.src.com.RFP_Batch225.Day2PracticeProblem;
+
+import java.util.Scanner;
+
+public class SumoFNumber {
+    public static void main(String[] args){
+
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter First N Number ");
+        int j = scan.nextInt();
+        int a = 0;
+        for (int i = 0; i < j; ++i){
+            System.out.println(a +=i);
+        }
+
+    }
+}
